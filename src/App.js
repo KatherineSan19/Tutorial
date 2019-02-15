5e6d1d3acadb76904f39abaab3220edb7b7002a5
KatherineSan19 <ktesan19@gmail.com>
2019-02-15 15:18:23 -0500

    practica con codigo facilito

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Titulo from './componentes/Titulo';
+import { hola } from './componentes/Titulo';
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+
+          <Titulo></Titulo>
+
           <a
             className="App-link"
             href="https://reactjs.org"
---
+++ src/componentes/Titulo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Titulo extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Places</h1>
+        <p>Descubre lugares de manera simple</p>
+        <p>¡Lugares únicos y maravillosos!</p>
+      </div>
+    );
+  }
+}
